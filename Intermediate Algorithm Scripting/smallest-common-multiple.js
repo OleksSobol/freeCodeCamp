3562439fc6a3c4987fef46293541585564b7ec64
OleksSobol <olekssobol@gmail.com>
2018-03-10 11:05:38 -0700

    FINALLY finished)

--- Intermediate Algorithm Scripting/smallest-common-multiple.js
+++ Intermediate Algorithm Scripting/smallest-common-multiple.js
@@ -1,62 +1,34 @@
-// working on it 
-//  1
-// 1 2 3 4 5 
-
-//  5
-//5 10 15 20 25 30 35 40 45 50 55 60
-
-// 1. start bigger number iteration and compare to smaller number 
-// 2. check if it is devided by first and 
-// second number
 
 function smallestCommons(arr) {
   
-  var arrFirst = [];
-  var arrSecond = [];
-  var bool = true;
+  var arrRange = [];
 
-  var bigger = Math.max(arr[0],arr[1]);
-  var smaller = Math.min(arr[0],arr[1]);;
-
-  var bigNum = bigger;
-  var smallNum = smaller;
-
-  function check(){
-     if (bigNum % smaller === 0 && smallNum % bigger === 0) {
-            bool = false;
-            arr = "Least Common Multiple is " + bigNum;
-            console.log(arr);  
-      }
+  for (var i = Math.max(arr[0],arr[1]); i >= Math.min(arr[0],arr[1]); i--) {
+   arrRange.push(i);
   }
-  
-  while(bool){
-      console.log("bigNum", bigNum);
-      console.log("smallNum", smallNum);
 
-    while (smallNum <= bigNum) {
-       smallNum += smaller;
-       arrSecond.push(smallNum);
-       check();
+  function lcm(arr){
+
+    return arr.reduce(function(accum, current){
+      return (accum * (current)) / gcd(accum, current); 
+    },arr[0]);
+  }
+
+  function gcd(a, b){
+    if (a === 0 ) {
+      return b;
     }
-
-     bigNum += bigger;
-     arrFirst.push(bigNum);
-     check();
-  
-
-    
-
-
-    // if (bigNum % smaller === 0 && smallNum % bigger ===0) {
-    //   bool = false;
-    //   arr = "Least Common Multiple is " + bigNum;
-    // }
-
-
+    if (b === 0 ) {
+      return a;
+    }
+    else{
+      return gcd(b, a%b);
+    }
   }
+ 
+
   
-  
-  return arr;
+  return lcm(arrRange);;
 }
 
 
